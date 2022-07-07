f0d4da7c430d3441db252348bec6e68d1a492271
xnjanx02 <anjana.jk009@gmail.com>
2022-07-08 00:16:02 +0530

    new component view-games added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddGamesComponent } from './add-games/add-games.component';
+import { ViewGamesComponent } from './view-games/view-games.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddGamesComponent
+    AddGamesComponent,
+    ViewGamesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-games/view-games.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-games',
+  templateUrl: './view-games.component.html',
+  styleUrls: ['./view-games.component.css']
+})
+export class ViewGamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
