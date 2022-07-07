7cb43d57546c8b115716ebdc9093ad21fddacf9d
xnjanx02 <anjana.jk009@gmail.com>
2022-07-08 00:02:06 +0530

    New component Add games added

---
+++ src/app/add-games/add-games.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-games',
+  templateUrl: './add-games.component.html',
+  styleUrls: ['./add-games.component.css']
+})
+export class AddGamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
